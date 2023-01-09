32631727d47a14e7ccb5a80904a3367dab0be4b9
Mahmudulmr19 <mahmudulmr19@gmail.com>
2023-01-09 23:12:13 +0600

    user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { auth } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "./firebase";
+// import { useAuthState } from "react-firebase-hooks/auth";
 
 const style = {
   appContainer: ` max-w-[728px] mx-auto text-center`,
   section: ` flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
 };
 function App() {
-  const [user] = useAuthState(auth);
-  // console.log(user);
+  // const [user] = useAuthState(auth);
   return (
     <div className={style.appContainer}>
       <section className={style.section}>
