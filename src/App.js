57422cf4287c7d958a12f4dacec17faf16a3ab09
Mahmudulmr19 <mahmudulmr19@gmail.com>
2023-01-10 12:54:21 +0600

    sdkngsd

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const style = {
-  appContainer: `max-w-[728px] mx-auto text-center`,
-  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+  appContainer: `max-w-[728px] mx-auto text-center `,
+  sectionContainer: `flex flex-col h-auto bg-gray-100 mt-10 shadow-xl border relative`,
 };
 
 function App() {
   const [user] = useAuthState(auth);
-  //  console.log(user)
+
   return (
     <div className={style.appContainer}>
-      <section className="{style.sectionContainer}">
+      <section className={style.sectionContainer}>
         {/* Navbar */}
         <Navbar />
         {user ? <Chat /> : null}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import LogOut from "./LogOut";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 const style = {
-  nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+  nav: `bg-gray-800 h-20 sticky top-0 z-50 flex justify-between items-center p-4`,
   heading: `text-white text-3xl`,
 };
 
